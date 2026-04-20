326429b2eba6f7eddd1f5acfbe34806f9584040f
Xiatsuma <xiatsuma@gmail.com>
2026-04-20 22:38:50 +0200

    Update QuantumRom.sh

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -13,6 +13,8 @@ chmod +x $(pwd)/bin/ext4/make_ext4fs
 chmod +x $(pwd)/bin/erofs-utils/extract.erofs
 chmod +x $(pwd)/bin/erofs-utils/mkfs.erofs
 
+# Source debloat system
+source "$(pwd)/scripts/debloat.sh"
 
 REMOVE_LINE() {
     if [ "$#" -ne 2 ]; then
@@ -26,7 +28,6 @@ REMOVE_LINE() {
     echo -e "- Deleting $LINE from $FILE"
     grep -vxF "$LINE" "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 }
-
 
 GET_PROP() {
     if [ "$#" -ne 3 ]; then
@@ -75,7 +76,6 @@ GET_PROP() {
     echo -e "$VALUE"
 }
 
-
 DOWNLOAD_FIRMWARE() {
     if [ "$#" -lt 4 ]; then
         echo -e "Usage: ${FUNCNAME[0]} <MODEL> <CSC> <IMEI> <DOWNLOAD_DIRECTORY> [VERSION]"
@@ -116,7 +116,6 @@ DOWNLOAD_FIRMWARE() {
     echo -e "- Firmware downloaded successfully! Size: ${file_size} MB"
     echo -e "- Saved to: $DOWN_DIR/firmware.zip"
 }
-
 
 EXTRACT_FIRMWARE() {
     if [ "$#" -ne 1 ]; then
@@ -192,7 +191,6 @@ EXTRACT_FIRMWARE() {
     fi
 }
 
-
 PREPARE_PARTITIONS() {
     if [ -z "$STOCK_DEVICE" ] || [ "$STOCK_DEVICE" = "None" ]; then
         export BUILD_PARTITIONS="odm,product,system_ext,system,vendor,odm_a,product_a,system_ext_a,system_a,vendor_a"
@@ -239,7 +237,6 @@ PREPARE_PARTITIONS() {
 
     shopt -u nullglob dotglob
 }
-
 
 EXTRACT_FIRMWARE_IMG() {
     echo -e ""
@@ -313,7 +310,6 @@ EXTRACT_FIRMWARE_IMG() {
     chmod -R u+rwX "$FIRM_DIR"
 }
 
-
 FIX_SYSTEM_EXT() {
     if [ "$#" -ne 1 ]; then
         echo -e "Usage: ${FUNCNAME[0]} <EXTRACTED_FIRM_DIR>"
@@ -376,7 +372,6 @@ FIX_SYSTEM_EXT() {
     fi
 }
 
-
 FIX_SELINUX() {
     if [ "$#" -ne 1 ]; then
         echo -e "Usage: ${FUNCNAME[0]} <EXTRACTED_FIRM_DIR>"
@@ -437,7 +432,6 @@ FIX_SELINUX() {
     REMOVE_LINE 'init.svc.vendor.wvkprov_server_hal                           u:object_r:wvkprov_prop:s0' "$TARGET_ROM_SYSTEM_EXT_DIR/etc/selinux/system_ext_property_contexts" >/dev/null 2>&1
 }
 
-
 FIX_VNDK() {
     echo -e "- Checking VNDK version."
     export SDK="$(GET_PROP "$EXTRACTED_FIRM_DIR" "system" ro.build.version.sdk)"
@@ -450,119 +444,6 @@ FIX_VNDK() {
         cp -rfa "$VNDKS_COLLECTION/$SDK/$STOCK_VNDK_VERSION/system_ext/"* "$TARGET_ROM_SYSTEM_EXT_DIR/"
     fi
 }
-
-
-###################################################################################################
-# DEBLOAT SYSTEM (STOCK CONFIG - RE-ENABLED)
-###################################################################################################
-
-DEBLOAT_APPS=(
-    "HMT" "PaymentFramework" "SamsungCalendar" "LiveTranscribe" "DigitalWellbeing" "Maps" "Duo" "Photos" "FactoryCameraFB" "WlanTest" "AssistantShell" "BardShell" "DuoStub" "GoogleCalendarSyncAdapter" "AndroidDeveloperVerifier" "AndroidGlassesCore" "SOAgent77" "YourPhone_Stub" "AndroidAutoStub" "SingleTakeService" "SamsungBilling" "AndroidSystemIntelligence" "GoogleRestore" "Messages" "SearchSelector" "AirGlance" "AirReadingGlass" "SamsungTTS" "WlanTest" "ARCore" "ARDrawing" "ARZone" "BGMProvider" "BixbyWakeup" "BlockchainBasicKit" "Cameralyzer" "DictDiotekForSec" "EasymodeContactsWidget81" "Fast" "FBAppManager_NS" "FunModeSDK" "GearManagerStub" "KidsHome_Installer" "LinkSharing_v11" "LiveDrawing" "MAPSAgent" "MdecService" "MinusOnePage" "MoccaMobile" "Netflix_stub" "Notes40" "ParentalCare" "PhotoTable" "PlayAutoInstallConfig" "SamsungPassAutofill_v1" "SmartReminder" "SmartSwitchStub" "UnifiedWFC" "UniversalMDMClient" "VideoEditorLite_Dream_N" "VisionIntelligence3.7" "VoiceAccess" "VTCameraSetting" "WebManual" "WifiGuider" "KTAuth" "KTCustomerService" "KTUsimManager" "LGUMiniCustomerCenter" "LGUplusTsmProxy" "SketchBook" "SKTMemberShip_new" "SktUsimService" "TWorld" "AirCommand" "AppUpdateCenter" "AREmoji" "AREmojiEditor" "AuthFramework" "AutoDoodle" "AvatarEmojiSticker" "AvatarEmojiSticker_S" "Bixby" "BixbyInterpreter" "BixbyVisionFramework3.5" "DevGPUDriver-EX2200" "DigitalKey" "Discover" "DiscoverSEP" "EarphoneTypeC" "EasySetup" "FBInstaller_NS" "FBServices" "FotaAgent" "GalleryWidget" "GameDriver-EX2100" "GameDriver-EX2200" "GameDriver-SM8150" "HashTagService" "MultiControlVP6" "LedCoverService" "LinkToWindowsService" "LiveStickers" "MemorySaver_O_Refresh" "MultiControl" "OMCAgent5" "OneDrive_Samsung_v3" "OneStoreService" "SamsungCarKeyFw" "SamsungPass" "SamsungSmartSuggestions" "SettingsBixby" "SetupIndiaServicesTnC" "SKTFindLostPhone" "SKTHiddenMenu" "SKTMemberShip" "SKTOneStore" "SktUsimService" "SmartEye" "SmartPush" "SmartThingsKit" "SmartTouchCall" "SOAgent7" "SOAgent75" "SolarAudio-service" "SPPPushClient" "sticker" "StickerFaceARAvatar" "StoryService" "SumeNNService" "SVoiceIME" "SwiftkeyIme" "SwiftkeySetting" "SystemUpdate" "TADownloader" "TalkbackSE" "TaPackAuthFw" "TPhoneOnePackage" "TPhoneSetup" "TWorld" "UltraDataSaving_O" "Upday" "UsimRegistrationKOR" "YourPhone_P1_5" "AvatarPicker" "GpuWatchApp" "KT114Provider2" "KTHiddenMenu" "KTOneStore" "KTServiceAgent" "KTServiceMenu" "LGUGPSnWPS" "LGUHiddenMenu" "LGUOZStore" "SKTFindLostPhoneApp" "SmartPush_64" "SOAgent76" "TService" "vexfwk_service" "VexScanner" "LiveEffectService" "YourPhone_P1_5" "vexfwk_service"
-)
-
-KICK() {
-    if [ "$#" -ne 1 ]; then
-        echo -e "Usage: ${FUNCNAME[0]} <EXTRACTED_FIRM_DIR>"
-        return 1
-    fi
-
-    local EXTRACTED_FIRM_DIR="$1"
-
-    echo -e "- Debloating apps."
-    local APP_DIRS=(
-        "$EXTRACTED_FIRM_DIR/system/system/app"
-        "$EXTRACTED_FIRM_DIR/system/system/priv-app"
-        "$EXTRACTED_FIRM_DIR/product/app"
-        "$EXTRACTED_FIRM_DIR/product/priv-app"
-    )
-
-    for app in "${DEBLOAT_APPS[@]}"; do
-        for dir in "${APP_DIRS[@]}"; do
-            target="$dir/$app"
-
-            if [[ -d "$target" ]]; then
-                rm -rf "$target" || echo -e "[WARN] Failed to remove $target"
-            fi
-        done
-    done
-}
-
-REMOVE_ESIM_FILES() {
-    if [ "$#" -ne 1 ]; then
-        echo -e "Usage: ${FUNCNAME[0]} <EXTRACTED_FIRM_DIR>"
-        return 1
-    fi
-
-    local EXTRACTED_FIRM_DIR="$1"
-    echo -e "- Removing ESIM files."
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/autoinstalls/autoinstalls-com.google.android.euicc"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/default-permissions/default-permissions-com.google.android.euicc.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/permissions/privapp-permissions-com.samsung.euicc.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/permissions/privapp-permissions-com.samsung.android.app.esimkeystring.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/permissions/privapp-permissions-com.samsung.android.app.telephonyui.esimclient.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/privapp-permissions-com.samsung.android.app.telephonyui.esimclient.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/sysconfig/preinstalled-packages-com.samsung.euicc.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/sysconfig/preinstalled-packages-com.samsung.android.app.esimkeystring.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/priv-app/EsimClient"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/priv-app/EsimKeyString"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/priv-app/EuiccService"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/priv-app/EuiccGoogle"
-}
-
-REMOVE_FABRIC_CRYPTO() {
-    if [ "$#" -ne 1 ]; then
-        echo -e "Usage: ${FUNCNAME[0]} <EXTRACTED_FIRM_DIR>"
-        return 1
-    fi
-
-    local EXTRACTED_FIRM_DIR="$1"
-    echo -e "- Removing fabric crypto."
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/bin/fabric_crypto"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/init/fabric_crypto.rc"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/permissions/FabricCryptoLib.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/vintf/manifest/fabric_crypto_manifest.xml"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/framework/FabricCryptoLib.jar"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/framework/oat/arm/FabricCryptoLib.odex"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/framework/oat/arm/FabricCryptoLib.vdex"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/framework/oat/arm64/FabricCryptoLib.odex"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/framework/oat/arm64/FabricCryptoLib.vdex"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/lib64/com.samsung.security.fabric.cryptod-V1-cpp.so"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/lib64/vendor.samsung.hardware.security.fkeymaster-V1-ndk.so"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/priv-app/KmxService"
-}
-
-DEBLOAT() {
-    if [ "$#" -ne 1 ]; then
-        echo -e "Usage: ${FUNCNAME[0]} <EXTRACTED_FIRM_DIR>"
-        return 1
-    fi
-
-    local EXTRACTED_FIRM_DIR="$1"
-    echo -e "${YELLOW}Debloating apps and files.${NC}"
-    
-    KICK "$EXTRACTED_FIRM_DIR"
-    REMOVE_ESIM_FILES "$EXTRACTED_FIRM_DIR"
-    REMOVE_FABRIC_CRYPTO "$EXTRACTED_FIRM_DIR"
-    
-    echo -e "- Deleting unnecessary files and folders."
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/app"/SamsungTTS*
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/init/boot-image.bprof"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/init/boot-image.prof"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/etc/mediasearch"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/hidden"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/preload"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/priv-app/MediaSearch"
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/priv-app"/GameDriver-*
-    rm -rf "$EXTRACTED_FIRM_DIR/system/system/tts"
-    rm -rf "$EXTRACTED_FIRM_DIR/product/app/Gmail2/oat"
-    rm -rf "$EXTRACTED_FIRM_DIR/product/app/Maps/oat"
-    rm -rf "$EXTRACTED_FIRM_DIR/product/app/SpeechServicesByGoogle/oat"
-    rm -rf "$EXTRACTED_FIRM_DIR/product/app/YouTube/oat"
-    rm -rf "$EXTRACTED_FIRM_DIR/product/priv-app"/HotwordEnrollment*
-    
-    echo -e "- Debloat complete"
-}
-
 
 ###################################################################################################
 # IMAGE BUILDING
@@ -628,7 +509,6 @@ GEN_FS_CONFIG() {
         echo -e "- $PARTITION fs_config generated"
     done
 }
-
 
 GEN_FILE_CONTEXTS() {
     if [ "$#" -ne 1 ]; then
@@ -707,7 +587,6 @@ GEN_FILE_CONTEXTS() {
         unset EXISTING
     done
 }
-
 
 BUILD_IMG() {
     if [ "$#" -ne 3 ]; then
