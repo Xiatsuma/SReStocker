c1980a8552d88c39535b8aa4cdd7d138d11ce6b9
Xiatsuma <xiatsuma@gmail.com>
2026-04-28 23:14:18 -0700

    Simplify script arguments and improve usage message

--- sixteen.sh
+++ sixteen.sh
@@ -1,53 +1,32 @@
 #!/bin/bash
 set -euo pipefail
-
-if [ "$#" -lt 6 ]; then
-    echo "Usage: $0 <STOCK_DEVICE> <USE_UI_8_TETHERING_APEX> <TARGET_DEVICE> <TARGET_DEVICE_CSC> <TARGET_DEVICE_IMEI> <OUTPUT_FILESYSTEM>"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <STOCK_DEVICE> <USE_UI_8_TETHERING_APEX> <OUTPUT_FILESYSTEM>"
     exit 1
 fi
-
-# Device info
 export STOCK_DEVICE="$1"
 export USE_UI_8_TETHERING_APEX="$2"
-export TARGET_DEVICE="$3"
-export TARGET_DEVICE_CSC="$4"
-export TARGET_DEVICE_IMEI="$5"
-export OUTPUT_FILESYSTEM="$6"
-
+export OUTPUT_FILESYSTEM="$3"
+export TARGET_DEVICE="$STOCK_DEVICE"
 if [[ "$OUTPUT_FILESYSTEM" != "erofs" && "$OUTPUT_FILESYSTEM" != "ext4" ]]; then
-    echo "OUTPUT_FILESYSTEM must be 'erofs' or 'ext4'. Got: $OUTPUT_FILESYSTEM"
+    echo "OUTPUT_FILESYSTEM must be erofs or ext4"
     exit 1
 fi
-
-VERSION="1"
-
-# Directories
+export VERSION="1"
 export OUT_DIR="$(pwd)/OUT"
 export WORK_DIR="$(pwd)/WORK"
 export FIRM_DIR="$(pwd)/FIRMWARE"
 export DEVICES_DIR="$(pwd)/QuantumROM/Devices"
 export APKTOOL="$(pwd)/bin/apktool/apktool.jar"
 export VNDKS_COLLECTION="$(pwd)/QuantumROM/vndks"
-
 export BUILD_PARTITIONS="product,system_ext,system"
-
-# Source
 source "$(pwd)/scripts/debloat.sh"
 source "$(pwd)/scripts/QuantumRom.sh"
-
 EXTRACT_FIRMWARE "$FIRM_DIR/$TARGET_DEVICE"
 EXTRACT_FIRMWARE_IMG "$FIRM_DIR/$TARGET_DEVICE"
-
 APPLY_STOCK_CONFIG "$FIRM_DIR/$TARGET_DEVICE"
-
 DEBLOAT "$FIRM_DIR/$TARGET_DEVICE"
 FIX_SELINUX "$FIRM_DIR/$TARGET_DEVICE"
 APPLY_CUSTOM_FEATURES "$FIRM_DIR/$TARGET_DEVICE"
-
 INSTALL_FRAMEWORK "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/framework-res.apk"
-
-#D_ID="$(grep -m1 '^ro.build.display.id=' "$FIRM_DIR/$TARGET_DEVICE/system/system/build.prop" | cut -d= -f2 | tr -d '\r')"
-#BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.build.display.id" "${D_ID} V-${VERSION}: Build with Quantum Tools"
-#BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "product" "ro.build.display.id" "${D_ID} V-${VERSION}: Build with Quantum Tools"
-
 BUILD_IMG "$FIRM_DIR/$TARGET_DEVICE" "$OUTPUT_FILESYSTEM" "$OUT_DIR"
